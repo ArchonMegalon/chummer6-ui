41e220863be6334d2065fab8cd19d51246a5be39
ArchonMegalon <archon.megalon@gmail.com>
2026-03-28 17:14:55 +0100

    Consume calmer workspace digests on desktop home

--- Chummer.Tests/Presentation/WorkspacePersistenceServiceTests.cs
+++ Chummer.Tests/Presentation/WorkspacePersistenceServiceTests.cs
@@ -191,6 +191,9 @@ public class WorkspacePersistenceServiceTests
         public Task<AccountCampaignSummary?> GetAccountCampaignSummaryAsync(CancellationToken ct)
             => Task.FromResult<AccountCampaignSummary?>(null);
 
+        public Task<IReadOnlyList<CampaignWorkspaceDigestProjection>> GetCampaignWorkspaceDigestsAsync(CancellationToken ct)
+            => Task.FromResult<IReadOnlyList<CampaignWorkspaceDigestProjection>>(Array.Empty<CampaignWorkspaceDigestProjection>());
+
         public Task<IReadOnlyList<DesktopHomeSupportDigest>> GetDesktopHomeSupportDigestsAsync(CancellationToken ct)
             => Task.FromResult<IReadOnlyList<DesktopHomeSupportDigest>>([]);
 
